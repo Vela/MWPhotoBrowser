43a9a4ea06b513d57199fa52304300394861a07d
Victor Xiong <pandaxcl@gmail.com>
2015-09-09 16:49:03 +0800

    1. Added a readonly property photoURL to access photo type MWPhoto. Mocha needed.

--- Pod/Classes/MWPhoto.h
+++ Pod/Classes/MWPhoto.h
@@ -17,6 +17,7 @@
 @interface MWPhoto : NSObject <MWPhoto>
 
 @property (nonatomic, strong) NSString *caption;
+@property (nonatomic, readonly) NSURL *photoURL;
 @property (nonatomic, strong) NSURL *videoURL;
 @property (nonatomic) BOOL emptyImage;
 @property (nonatomic) BOOL isVideo;
